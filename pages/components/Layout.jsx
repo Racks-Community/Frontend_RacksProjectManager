37d8146fd3c149af90df13f0302b6b9ebff5bcae
KaladinStormBlessed <davidcr.odalys@gmail.com>
2023-05-15 19:24:47 +0200

    Page Title

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -1,9 +1,18 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Racks Community</title>
+        <meta
+          property="og:title"
+          content="Racks Community</title>"
+          key="title"
+        />
+      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
